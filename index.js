f4009abe1cbc52af1c275f0b90dcec9be1e27617
ankitdixit <ankitdixit29269@gmail.com>
2023-07-23 10:44:20 +0530

    some modifications

--- index.js
+++ index.js
@@ -26,3 +26,45 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
 sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+
+// doflf
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event) => {
+  const { name, email, subject, message } = JSON.parse(event.body);
+
+  // Setup the email transport
+  const credentials = process.env.GMAIL_CREDENTIALS.split(':');
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: credentials[0], // Extract the username
+      pass: credentials[1], // Extract the password
+    },
+  });
+
+  try {
+    // Send the email
+    await transporter.sendMail({
+      from: email,
+      to: "portfolio.web87@gmail.com", // Replace with your email address
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    return {
+      statusCode: 200,
+      body: "Email sent successfully!",
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: "Failed to send the email. Please try again later.",
+    };
+  }
+};
+
